d32b87f1d43a68e4d5c17efee00a93582b50e52a
bokmann <dbock@codesherpas.com>
2010-04-21 18:26:23 -0400

    improving the generated rdoc

--- lib/business_time/config.rb
+++ lib/business_time/config.rb
@@ -1,12 +1,26 @@
 module BusinessTime
   
-  # controls the behavior of the code.  You can change
+  # controls the behavior of this gem.  You can change
   # the beginning_of_workday, end_of_workday, and the list of holidays
-  #manually, or with a yaml file and the load method.
+  # manually, or with a yaml file and the load method.
   class Config
     class << self
+      # You can set this yourself, either by the load method below, or
+      # by saying
+      #   BusinessTime::Config.beginning_of_workday = "8:30 am"
+      # someplace in the initializers of your application.
       attr_accessor :beginning_of_workday
+      
+      # You can set this yourself, either by the load method below, or
+      # by saying
+      #   BusinessTime::Config.end_of_workday = "5:30 pm"
+      # someplace in the initializers of your application.
       attr_accessor :end_of_workday
+      
+      # You can set this yourself, either by the load method below, or
+      # by saying
+      #   BusinessTime::Config.holidays << my_holiday_date_object
+      # someplace in the initializers of your application.
       attr_accessor :holidays
     end
     
@@ -17,13 +31,14 @@ module BusinessTime
     end
     
     # loads the config data from a yaml file written as:
-    # business_time:
-    #   beginning_od_workday: 8:30 am
-    #   end_of_workday: 5:30 pm
-    #   holidays:
-    #     - Jan 1st, 2010
-    #     - July 4th, 2010
-    #     - Dec 25th, 2010
+    #
+    #   business_time:
+    #     beginning_od_workday: 8:30 am
+    #     end_of_workday: 5:30 pm
+    #     holidays:
+    #       - Jan 1st, 2010
+    #       - July 4th, 2010
+    #       - Dec 25th, 2010
     def self.load(filename)
       self.reset
       data = YAML::load(File.open(filename))
