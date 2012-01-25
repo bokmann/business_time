895a4926d2b22d7a1f3d868efbcd87f6f3f6f2f8
Glenn Vanderburg <glv@vanderburg.org>
2012-01-25 12:18:15 -0600

    Someone mixed up expected and actual ...

--- test/test_config.rb
+++ test/test_config.rb
@@ -3,15 +3,15 @@ require 'helper'
 class TestConfig < Test::Unit::TestCase
   
   should "keep track of the start of the day" do
-    assert_equal BusinessTime::Config.beginning_of_workday, "9:00 am"
+    assert_equal "9:00 am", BusinessTime::Config.beginning_of_workday
     BusinessTime::Config.beginning_of_workday = "8:30 am"
-    assert_equal BusinessTime::Config.beginning_of_workday, "8:30 am"
+    assert_equal "8:30 am", BusinessTime::Config.beginning_of_workday
   end
   
   should "keep track of the end of the day" do
-    assert_equal BusinessTime::Config.end_of_workday, "5:00 pm"
+    assert_equal "5:00 pm", BusinessTime::Config.end_of_workday
     BusinessTime::Config.end_of_workday = "5:30 pm"
-    assert_equal BusinessTime::Config.end_of_workday, "5:30 pm"
+    assert_equal "5:30 pm", BusinessTime::Config.end_of_workday
   end
   
   should "keep track of holidays" do
@@ -21,5 +21,5 @@ class TestConfig < Test::Unit::TestCase
     BusinessTime::Config.holidays << daves_birthday
     assert BusinessTime::Config.holidays.include?(daves_birthday)
   end
-  
+    
 end
