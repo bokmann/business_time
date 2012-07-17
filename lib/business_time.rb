d33419c5b258a26040c5287af8ba2789486db9a3
Harry Marr <harry.marr@gmail.com>
2012-07-17 12:03:57 +0100

    Move lib/extensions to lib/business_time/core_ext

--- lib/business_time.rb
+++ lib/business_time.rb
@@ -1,10 +1,12 @@
 require 'thread'
-require "active_support"
+require 'active_support'
 require 'active_support/time'
-require "time"
-require "business_time/config"
-require "business_time/business_hours"
-require "business_time/business_days"
-require "extensions/date"
-require "extensions/time"
-require "extensions/fixnum"
+require 'time'
+
+require 'business_time/config'
+require 'business_time/business_hours'
+require 'business_time/business_days'
+require 'business_time/core_ext/date'
+require 'business_time/core_ext/time'
+require 'business_time/core_ext/fixnum'
+
