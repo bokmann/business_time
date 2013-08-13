ca45265291ab4a42804ffbbe222e9e0bf4841b9e
Daniel Lo <daniel.lo@lawgives.com>
2013-08-13 05:10:26 +0000

    rfc822 isn't available in 1.9.3, changed to strftime for compatability

--- lib/business_time/business_days.rb
+++ lib/business_time/business_days.rb
@@ -8,7 +8,7 @@ module BusinessTime
     end
 
     def after(time = Time.now)
-      time = Time.zone ? Time.zone.parse(time.rfc822) : Time.parse(time.rfc822)
+      time = Time.zone ? Time.zone.parse(time.strftime('%Y-%m-%d %H:%M:%S %z')) : Time.parse(time.strftime('%Y-%m-%d %H:%M:%S %z'))
       days = @days
       while days > 0 || !Time.workday?(time)
         days -= 1 if Time.workday?(time)
