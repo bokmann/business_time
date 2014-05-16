3062b9a2b3abdd3abb743cc86d0cdab7cabe5a51
Stephane Bounmy <stephanebounmy@gmail.com>
2014-05-16 18:04:19 +0200

    remove unecessary override

--- lib/business_time/core_ext/active_support/time_with_zone.rb
+++ lib/business_time/core_ext/active_support/time_with_zone.rb
@@ -1,8 +1,3 @@
 class ActiveSupport::TimeWithZone
   include BusinessTime::TimeExtensions
-
-  def business_time_until(to_time)
-    to_time = to_time.time if to_time.respond_to? :time
-    self.time.business_time_until(to_time)
-  end
 end
